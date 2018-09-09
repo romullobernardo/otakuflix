df563ed823980ced5d549638de760850a6f99efc
romullobernardo <romulloqueiroz@gmail.com>
2018-09-09 19:29:46 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment }             from 'react'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider }                               from 'react-redux'    
 
 import Home                                       from './components/Home'
